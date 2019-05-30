9f9b4664ff1f7ded3ea9c8399739ce5361c15898
ReMantano <kondrusevvladislav@gmail.com>
2019-05-30 14:15:51 +0300

    Added userController

--- timetracker-backend/src/main/java/by/ts/tt/entiry/User.java
+++ timetracker-backend/src/main/java/by/ts/tt/entiry/User.java
@@ -1,21 +1,25 @@
 package by.ts.tt.entiry;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+import static by.ts.tt.utilities.UserTableConstants.*;
 
 @Entity
-@Table(name = "users")
-public class User {
+@Table(name = USER_TABLE)
+public class User implements Serializable {
 
     @Id
-    @Column(name = "u_id")
+    @Column(name = USER_ID)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "u_name")
+    @Column(name = USER_NAME)
     private String name;
 
 
-    @Column(name = "u_vacation")
+    @Column(name = USER_VACATION)
     private int vacation;
 
     public int getId() {
@@ -30,15 +34,35 @@ public class User {
         return vacation;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
 
     public void setVacation(int vacation) {
         this.vacation = vacation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                vacation == user.vacation &&
+                name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, vacation);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", vacation=" + vacation +
+                '}';
     }
 }
