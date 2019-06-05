77830a443c05232775979beade9735e21804bda5
ReMantano <kondrusevvladislav@gmail.com>
2019-06-05 17:42:00 +0300

    Added swagger

--- timetracker-backend/src/main/java/by/ts/tt/controller/UserController.java
+++ timetracker-backend/src/main/java/by/ts/tt/controller/UserController.java
@@ -4,14 +4,26 @@ import by.ts.tt.DTO.UserVacationDTO;
 import by.ts.tt.service.impl.UserServiceImpl;
 import by.ts.tt.service.impl.VacationServiceImpl;
 import by.ts.tt.utilities.ResponseMessage;
+import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.sql.Date;
 import java.util.List;
 
+@SwaggerDefinition(
+        info = @Info(
+                description = "Rest service for user vacations",
+                version = "1.0.0",
+                title = "Swagger user controller"
+        ),
+        consumes = "application/json",
+        produces = "application/json",
+        tags = {@Tag(name="user", description = "Operations about users")}
+)
 
 @RestController
+@Api(value = "User controller")
 public class UserController {
 
     @Resource
@@ -20,6 +32,11 @@ public class UserController {
     @Resource
     VacationServiceImpl vacationService;
 
+    @ApiOperation(  httpMethod = "GET",
+                    value = "Get a list of users with vacations",
+                    response = UserVacationDTO.class,
+                    nickname = "getUserVacation",
+                    produces = "application/json")
     @GetMapping("/users")
     public ResponseEntity<ResponseMessage> getUserVacation(){
         List<UserVacationDTO> list = userService.getUserVacation();
@@ -27,18 +44,32 @@ public class UserController {
         return ResponseEntity.ok().body(new ResponseMessage(list));
     }
 
+    @ApiOperation(  httpMethod = "GET" ,
+                    value = "Get a sorted list of users by start date and end date of vacation",
+                    response = UserVacationDTO.class,
+                    nickname = "getSortedUserByDate",
+                    produces = "application/json")
     @GetMapping("/users/sort")
-    public ResponseEntity<ResponseMessage> getSortedUserByDate(@RequestParam(value = "start")  String start,@RequestParam(value = "end") String end){
+    public ResponseEntity<ResponseMessage> getSortedUserByDate(     @RequestParam(value = "start")
+                                                                    @ApiParam(value = "Start vacation", required = true) String start,
+                                                                    @ApiParam(value = "End vacation",required = true)
+                                                                    @RequestParam(value = "end") String end){
+
         List<UserVacationDTO> list = userService.sortUsersByDate(new Date(Long.parseLong(start)), new Date(Long.parseLong(end)));
 
         return ResponseEntity.ok().body(new ResponseMessage(list));
     }
 
 
+    @ApiOperation(  httpMethod = "POST" ,
+            value = "Update users",
+            nickname = "updateUser",
+            produces = "application/json")
     @PostMapping("/user/update")
-    public ResponseEntity<ResponseMessage> updateUser(@RequestBody UserVacationDTO uv){
-        userService.updateUser(uv.getUser());
-        vacationService.updateVacation(uv.getVacation());
+    public ResponseEntity<ResponseMessage> updateUser(    @ApiParam(value = "Updated user object", required = true)
+                                                          @RequestBody UserVacationDTO uservVacation){
+        userService.updateUser(uservVacation.getUser());
+        vacationService.updateVacation(uservVacation.getVacation());
 
         return ResponseEntity.ok(new ResponseMessage(null));
     }
